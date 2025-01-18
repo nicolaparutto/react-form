6941d4721d5c90f8018ca8a10f4935a523f0ce6f
nicolaparutto <nicolaparutto2003@gmail.com>
2025-01-18 17:27:10 +0100

    feat: add article deletion system

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -2,24 +2,21 @@ import { useState } from "react";
 const articles = [
    {
       id: 1,
-      title:"Temperature da record!",
       text: "Oggi si è registrato un record storico di temperature in Europa, con 20 gradi sopra la media stagionale."
    },
    {
       id: 2,
-      title:"Nuovi studi importanti",
       text: "Un nuovo studio scientifico rivela che l'intelligenza artificiale potrebbe rivoluzionare la diagnosi delle malattie rare."
    },
    {
       id: 3,
-      title:"Nuova missione spaziale",
       text: "La NASA ha annunciato il lancio di una nuova missione verso Marte per cercare tracce di vita passata."
    }
 ]
 function Form() {
    const [articlesList, setArticlesList] = useState(articles);
-   const [newArticle, setNewArticle] = useState({title: ''})
-
+   const [newArticle, setNewArticle] = useState({text: ''})
+   const nextId = 3;
    const handlerSubmit = (e) => {
       e.preventDefault();
       const newArticlesList = [newArticle, ...articlesList]
@@ -27,30 +24,40 @@ function Form() {
    }
    const handlerNewArticle = (e) => {
       const newArticle = {
-         id: 4,
-         title: e.target.value
+         id: Date.now(),
+         text: e.target.value
       }
       setNewArticle(newArticle);
    }
-
+   const handlerRemoveArticle = (id) => {
+      const updatedArticleList = articlesList.filter(article =>(article.id !== id))
+      setArticlesList(updatedArticleList)
+   }
 
    return (
      <div className="container form-container">
       <form action="" onSubmit={handlerSubmit}>
          <input 
          type="text" 
-         placeholder="Titolo dell'articolo"
+         placeholder="Inserisci il testo dell'articolo"
          value={newArticle.text}
          onChange={handlerNewArticle}
          required/>
-         <button className="btn" type="submit">Invia</button>
+         <button className="btn" type="submit">Genera</button>
       </form>
 
       <div className="articles-section">
          {articlesList.map((article) => (
                <div key={article.id} className="article">
-                  <h1>{article.title}</h1>
-                  <p>{article.text}</p>
+                  <div className="article-text">
+                     <p>{article.text}</p>
+                  </div>
+                  <div
+                  className="article-trash"
+                  onClick={() => handlerRemoveArticle(article.id)}
+                  >
+                     <i className="fa-solid fa-trash"></i>
+                  </div>
                </div>
          ))}
       </div>
