4f2ba566fd8c5e2395d017124c4d451b33305a3e
nicolaparutto <nicolaparutto2003@gmail.com>
2025-01-18 16:12:07 +0100

    feat: possibility, when submitting, to generate a new article

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -1,11 +1,39 @@
+import { useState } from "react";
+const articles = [
+   "articolo 1",
+   "articolo 2",
+   "articolo 3"
+]
 function Form() {
+   const [articlesList, setArticlesList] = useState(articles);
+   const [newArticle, setNewArticle] = useState("")
+
+   const handlerSubmit = (e) => {
+      e.preventDefault();
+      const newArticlesList = [newArticle, ...articlesList]
+      setArticlesList(newArticlesList)
+   }
 
    return (
      <div className="container form-container">
-      <form action="">
-         <input type="text" placeholder="Inserisci il titolo..."/>
-         <button className="btn">Invia</button>
+      <form action="" onSubmit={handlerSubmit}>
+         <input 
+         type="text" 
+         placeholder="Titolo dell'articolo"
+         value={newArticle}
+         onChange={(e) => setNewArticle( e.target.value)}
+         required/>
+         <button className="btn" type="submit">Invia</button>
       </form>
+
+      <div className="articles-section">
+         {articlesList.map((article, index) => (
+               <div key={index} className="article">
+                  <h1>{article}</h1>
+                  <p>testo dell'articolo</p>
+               </div>
+         ))}
+      </div>
      </div>
    )
  }
